3b9137fdeec14d652fe02b880b8d248f854ee432
coffeelack <98415174+coffeelack@users.noreply.github.com>
2023-10-30 09:56:51 +0100

    Dynamic result fild added, button to open path added

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 from openpyxl import load_workbook
 
-global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry, search_inside_files
+global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry, search_inside_files, indexed_folder_label
+
 
 def index_folder():
     global indexed_folder, indexed_files, indexed_dirs
@@ -14,6 +15,8 @@ def index_folder():
         indexed_dirs = 0
         index_files_in_folder(folder_path)
         result_label.config(text=f"Indexed {indexed_files} files and {indexed_dirs} directories.")
+        indexed_folder_label.config(text=f"Indexed Folder: {indexed_folder}")
+
 
 def index_files_in_folder(folder):
     global indexed_files, indexed_dirs
@@ -24,6 +27,7 @@ def index_files_in_folder(folder):
         elif os.path.isdir(item_path):
             indexed_dirs += 1
             index_files_in_folder(item_path)
+
 
 def search_files(result_listbox):
     search_term = search_entry.get().lower()
@@ -40,7 +44,7 @@ def search_files(result_listbox):
     for item in os.listdir(indexed_folder):
         item_path = os.path.join(indexed_folder, item)
         if search_term in item.lower() and os.path.isfile(item_path) and item_path.endswith(('.xls', '.xlsx')):
-            result_listbox.insert(tk.END, item)
+            result_listbox.insert(tk.END, f"File: {item} | Path: {item_path}")
             found = True
 
         if search_inside_files.get() == 1 and os.path.isfile(item_path) and item_path.endswith(('.xls', '.xlsx')):
@@ -50,7 +54,7 @@ def search_files(result_listbox):
                     for row in sheet.iter_rows():
                         for cell in row:
                             if search_term in str(cell.value).lower():
-                                result_listbox.insert(tk.END, item)
+                                result_listbox.insert(tk.END, f"File: {item} | Path: {item_path}")
                                 found = True
                                 break
             except Exception as e:
@@ -59,15 +63,34 @@ def search_files(result_listbox):
     if not found:
         messagebox.showinfo("No Matches Found", "No files matching the search term were found.")
 
+    # Resize the result_listbox
+    num_results = result_listbox.size()
+    if num_results > 10:
+        result_listbox.configure(height=10)
+    else:
+        result_listbox.configure(height=num_results)
+
+
 def open_file(result_listbox=None):
-    selected_file = result_listbox.get(tk.ACTIVE)
-    if selected_file:
-        os.startfile(os.path.join(indexed_folder, selected_file))
+    selected_item = result_listbox.get(tk.ACTIVE)
+    if selected_item:
+        file_path = selected_item.split("| Path: ")[-1].strip()
+        os.startfile(file_path)
     else:
         messagebox.showinfo("No File Selected", "Please select a file to open.")
 
+
+def open_path(result_listbox=None):
+    selected_item = result_listbox.get(tk.ACTIVE)
+    if selected_item:
+        file_path = selected_item.split("| Path: ")[-1].strip()
+        os.startfile(os.path.dirname(file_path))
+    else:
+        messagebox.showinfo("No File Selected", "Please select a file to open the path.")
+
+
 def main():
-    global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry, search_inside_files
+    global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry, search_inside_files, indexed_folder_label
 
     # Create GUI
     root = tk.Tk()
@@ -76,6 +99,9 @@ def main():
 
     index_button = tk.Button(root, text="Index Folder", command=index_folder)
     index_button.pack()
+
+    indexed_folder_label = tk.Label(root, text="")
+    indexed_folder_label.pack()
 
     search_label = tk.Label(root, text="Search:")
     search_label.pack()
@@ -91,10 +117,16 @@ def main():
     search_button.pack()
 
     result_listbox = tk.Listbox(root)
-    result_listbox.pack()
+    result_listbox.pack(expand=True, fill=tk.BOTH)
 
-    open_button = tk.Button(root, text="Open", command=lambda: open_file(result_listbox))
-    open_button.pack()
+    button_frame = tk.Frame(root)
+    button_frame.pack()
+
+    open_file_button = tk.Button(button_frame, text="Open File", command=lambda: open_file(result_listbox))
+    open_file_button.pack(side=tk.LEFT)
+
+    open_path_button = tk.Button(button_frame, text="Open Path", command=lambda: open_path(result_listbox))
+    open_path_button.pack(side=tk.LEFT)
 
     quit_button = tk.Button(root, text="Quit", command=root.quit)
     quit_button.pack()
@@ -107,6 +139,7 @@ def main():
     indexed_dirs = 0
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
