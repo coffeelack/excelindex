ff85ec31edcdd816716d9a538fbcd436e4ec22d7
coffeelack <98415174+coffeelack@users.noreply.github.com>
2023-10-30 09:37:47 +0100

    Searching filenames working

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 
-global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry
+global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry, search_inside_files
+
 
 def index_folder():
     global indexed_folder, indexed_files, indexed_dirs
@@ -14,6 +15,7 @@ def index_folder():
         index_files_in_folder(folder_path)
         result_label.config(text=f"Indexed {indexed_files} files and {indexed_dirs} directories.")
 
+
 def index_files_in_folder(folder):
     global indexed_files, indexed_dirs
     for item in os.listdir(folder):
@@ -24,21 +26,46 @@ def index_files_in_folder(folder):
             indexed_dirs += 1
             index_files_in_folder(item_path)
 
+
 def search_files(result_listbox):
     search_term = search_entry.get().lower()
+    if not search_term:
+        messagebox.showinfo("Search Term Missing", "Please enter a search term.")
+        return
+
+    if not indexed_folder:
+        messagebox.showinfo("Folder Not Indexed", "Please index a folder first.")
+        return
+
     result_listbox.delete(0, tk.END)
+    found = False
     for item in os.listdir(indexed_folder):
         item_path = os.path.join(indexed_folder, item)
         if search_term in item.lower() and os.path.isfile(item_path) and item_path.endswith(('.xls', '.xlsx')):
             result_listbox.insert(tk.END, item)
+            found = True
+
+        if search_inside_files.get() == 1 and os.path.isfile(item_path) and item_path.endswith(('.xls', '.xlsx')):
+            with open(item_path, 'r', encoding='utf-8', errors='ignore') as file:
+                content = file.read()
+                if search_term in content.lower():
+                    result_listbox.insert(tk.END, item)
+                    found = True
+
+    if not found:
+        messagebox.showinfo("No Matches Found", "No files matching the search term were found.")
+
 
 def open_file(result_listbox=None):
     selected_file = result_listbox.get(tk.ACTIVE)
     if selected_file:
         os.startfile(os.path.join(indexed_folder, selected_file))
+    else:
+        messagebox.showinfo("No File Selected", "Please select a file to open.")
+
 
 def main():
-    global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry
+    global indexed_folder, indexed_files, indexed_dirs, result_label, search_entry, search_inside_files
 
     # Create GUI
     root = tk.Tk()
@@ -54,13 +81,17 @@ def main():
     search_entry = tk.Entry(root)
     search_entry.pack()
 
-    search_button = tk.Button(root, text="Search", command=search_files)
+    search_inside_files = tk.IntVar()  # Variable to hold checkbox state
+    search_inside_checkbox = tk.Checkbutton(root, text="Search Inside Files", variable=search_inside_files)
+    search_inside_checkbox.pack()
+
+    search_button = tk.Button(root, text="Search", command=lambda: search_files(result_listbox))
     search_button.pack()
 
     result_listbox = tk.Listbox(root)
     result_listbox.pack()
 
-    open_button = tk.Button(root, text="Open", command=open_file)
+    open_button = tk.Button(root, text="Open", command=lambda: open_file(result_listbox))
     open_button.pack()
 
     quit_button = tk.Button(root, text="Quit", command=root.quit)
@@ -74,6 +105,7 @@ def main():
     indexed_dirs = 0
 
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
